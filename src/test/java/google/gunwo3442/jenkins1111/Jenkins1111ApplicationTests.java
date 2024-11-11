f816136865288b40e3a5b08054700b65bd74898e
gunwoda <gunwo3442@gmail.com>
2024-11-11 10:22:20 +0900

    init

---
+++ src/test/java/google/gunwo3442/jenkins1111/Jenkins1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package google.gunwo3442.jenkins1111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins1111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
